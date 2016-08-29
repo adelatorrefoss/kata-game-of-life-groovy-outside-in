e6d9602cdbe6876b292902d2ab0fceab2131b35f
Antonio de la Torre <antondelatorre@gmail.com>
2016-08-29 09:00:11 +0200

    RED: return new grid same size

--- src/test/groovy/NextServiceImplSpec.groovy
+++ src/test/groovy/NextServiceImplSpec.groovy
@@ -2,6 +2,7 @@ import spock.lang.Specification
 
 import GameOfLife.Grid
 import GameOfLife.NextServiceImpl
+import GameOfLife.GridItem
 
 class NextServiceImplSpec extends Specification {
 
@@ -37,7 +38,20 @@ class NextServiceImplSpec extends Specification {
         (1.._) * seed.next() >> null
     }
 
+    void 'should create new grid with the same size as seed'() {
+        given:
+        NextServiceImpl nextService = createNextService()
+        Grid seed = Mock(Grid)
 
+        when:
+        Grid next = nextService.next(seed)
+
+        then:
+        // TODO??? How to check size without show internals of Grid to NextService
+        // next.size() == seed.size()
+        (1..5) * seed.next() >>> [new GridItem(), new GridItem(), new GridItem(), new GridItem(), null]
+        (1..5) * seed.push(_)
+    }
 
     /******
      * TODO
