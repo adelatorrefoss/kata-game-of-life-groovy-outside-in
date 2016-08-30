03d74731d59359a5ae073ea78b9e0b8c862ebca5
Antonio de la Torre <antondelatorre@gmail.com>
2016-08-30 13:50:02 +0200

    GREEN: should not finish with a line

--- src/test/groovy/GameOfLifeIntegrationSpec.groovy
+++ src/test/groovy/GameOfLifeIntegrationSpec.groovy
@@ -110,4 +110,31 @@ class GameOfLifeIntegrationSpec extends Specification {
         result.iterations == 4
     }
 
+    void 'should not finish grid with a line'() {
+        given:
+        def golAlg = new GolAlgorithmImpl()
+        def nextService = new NextServiceImpl(golAlg)
+        def gol = new GameOfLife(nextService)
+        def seed = new Grid(3,3)
+        seed.push(new GridItem(false))
+        seed.push(new GridItem(false))
+        seed.push(new GridItem(false))
+        seed.push(new GridItem(true))
+        seed.push(new GridItem(true))
+        seed.push(new GridItem(true))
+        seed.push(new GridItem(false))
+        seed.push(new GridItem(false))
+        seed.push(new GridItem(false))
+        int maxIterations = 10
+
+        when:
+        def result = gol.run(seed, maxIterations)
+        def resultBooleans = result.grid.getData().collect { it.isAlive() }
+        def seedBooleans = seed.getData().collect { it.isAlive() }
+
+        then:
+        result.grid.getData().any { it.isAlive() }
+        result.iterations == 10
+        resultBooleans.equals(seedBooleans)
+    }
 }
